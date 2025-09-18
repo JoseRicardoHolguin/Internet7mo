0818502801d8e0f14687d56c354dc586c2ae0a01
Jose Holguin <13375@ulsachihuahua.edu>
2025-09-17 19:55:20 -0600

    Nuevo Views con funcionalidades

--- proyectojose/Jose_App/views.py
+++ proyectojose/Jose_App/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
 
 def index(request):
-	return HttpResponse("Hello, World!")
+    resultado = None  # valor inicial
+
+    if request.method == "POST":
+        num1 = int(request.POST.get("num1", 0))
+        num2 = int(request.POST.get("num2", 0))
+        resultado = num1 + num2  # operación: suma
+
+    return render(request, "Jose_App/index.html", {"resultado": resultado})
