bc8a4e71e4362f11f6678922c6f70fddec99aa0a
Jose Holguin <13375@ulsachihuahua.edu>
2025-09-22 18:56:08 -0600

    mas cambios a htmls

--- proyectojose/Jose_App/views.py
+++ proyectojose/Jose_App/views.py
@@ -19,7 +19,7 @@ def ruido(request):
 def random_number(request):
     import random
     numero_aleatorio = random.randint(1, 100)
-    return render(request, "Jose_App/random_number.html", {"numero": numero_aleatorio})
+    return render(request, "Jose_App/random.html", {"numero": numero_aleatorio})
 
 def saludo(request, nombre):
     return render(request, "Jose_App/saludo.html", {"nombre": nombre.capitalize()})
