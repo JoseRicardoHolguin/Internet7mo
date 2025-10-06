07f2611935babfea5d40ae149f6e40543516ef65
Jose Holguin <13375@ulsachihuahua.edu>
2025-10-06 17:17:51 -0600

    se agrega usuario

--- proyectojose/Jose_App/models.py
+++ proyectojose/Jose_App/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Task(models.Model):
     title = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.PositiveIntegerField()
